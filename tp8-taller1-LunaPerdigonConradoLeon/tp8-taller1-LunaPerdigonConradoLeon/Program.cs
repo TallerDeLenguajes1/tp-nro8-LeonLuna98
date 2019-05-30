f5d6760756d365895f92271a20cbc01daf1fe39d
LeonLuna98 <you@example.com>
2019-05-29 22:43:34 -0300

    Primera subida

---
+++ tp8-taller1-LunaPerdigonConradoLeon/tp8-taller1-LunaPerdigonConradoLeon/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp8_taller1_LunaPerdigonConradoLeon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            /*
+            Empleado miempleado = new Empleado("Juan","Lopez");
+            miempleado.nombre=  "Jorge";*/
+
+            string rutadelarchivo = @"./tp8-taller1-LunaPerdigonConradoLeon/ListaDeEmpleados.csv";
+            string losempleados = File.ReadAllText(rutadelarchivo);
+
+            Console.WriteLine("Lista de empleados {0}",losempleados);
+
+
+            List<string> lalista = File.ReadAllLines(@".\tp8-taller1-LunaPerdigonConradoLeon\ListaDeEmpleados.csv").ToList();//Mejor un arreglo(?
+            System.Console.WriteLine();
+
+            //Que muestre los valores de la lista
+            foreach (string linea in lalista)
+            {
+                var valores= linea.Split(';');
+                Console.WriteLine("Nombre: " + valores[0] + "Apellido:" + valores[1] + "Estado Civil: " + valores[3] + "Sueldo: " + valores[4] + "Genero:" + valores[5] );
+
+            }
+
+            string[] discos = System.IO.Directory.GetLogicalDrives();
+            foreach(string eldisco in discos)
+            {
+
+                Console.WriteLine("Disponible el disco:" + eldisco);
+
+
+            }
+
+            Console.WriteLine("En que disco desea guardar? ");
+            string discoelegido = Console.ReadLine();
+            //File.Exists("C:/BackUpAgenda.bk ");
+            if (discoelegido.Equals(discos[0]))
+            {
+                if (File.Exists(discos[0]))
+                {
+                    //File.Copy("archivoorigonal", "archivodestino.nuevaextension")
+                    File.Copy(rutadelarchivo, @"C:\BackUpAgenda.csv");//Suponiendo que es el C
+
+                    System.IO.File.Move(@"C:\BackUpAgenda.csv", @"C:\BackUpAgenda.bk");
+                }
+                else
+                {
+                    string elbackup = @"C:\ BackUpfile.csv";
+                    FileStream backup = File.Create(elbackup);
+                }
+                    
+
+            }
+
+
+        }
+    }
+}
---
+++ tp8-taller1-LunaPerdigonConradoLeon/tp8-taller1-LunaPerdigonConradoLeon/empleado.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp8_taller1_LunaPerdigonConradoLeon
+{
+
+    public enum elcargo
+    {
+        Auxiliar, Administrativo, Ingeniero, Especialista, Investigador
+    }
+    public enum elgenero
+    {
+        Masculino, Femenino
+    }
+    public enum elestadocivil
+    {
+        Soltero, Casado, Viudo, Otro
+    }
+
+    /*public struct fechas
+    {
+        int dia;
+        int mes;
+        int anio;
+
+    }
+    */
+
+
+    public struct Empleado
+    {
+
+        private string nombre;
+        string apellido;
+        elestadocivil estadocivil;
+        elgenero genero;
+        elcargo cargo;
+       /* fechas fechanacimiento;
+        fechas fechaingreso;*/
+        double sueldo;
+
+
+       
+        public Empleado(string _nombre, string _apellido, elestadocivil _estadocivil, double _sueldo, elgenero _genero, elcargo _cargo/*, fechas _fechanacimiento, fechas _fechaingreso*/)
+        {
+            nombre = _nombre;
+            apellido = _apellido;
+            estadocivil = _estadocivil;
+            cargo = _cargo;
+            genero = _genero;
+            sueldo = _sueldo;
+           /* fechanacimiento = _fechanacimiento;
+            fechaingreso = _fechaingreso;*/
+
+        }
+
+     
+        public void mostrarcontacto()
+        {
+            Console.WriteLine(nombre);
+            Console.WriteLine(apellido);
+            Console.WriteLine(estadocivil);
+            Console.WriteLine(genero);
+            Console.WriteLine(cargo);
+            Console.WriteLine(sueldo);
+            /*Console.WriteLine(@"{0}  / {1} / {2}", fechanacimiento.dia, fechanacimiento.mes, fechanacimiento.anio);
+            Console.WriteLine(@"{0}  / {1}  / {2}", fechaingreso.dia, fechaingreso.mes, fechaingreso.anio);*/
+            Console.WriteLine("--------------");
+        }
+
+
+        //el fin de la estructura
+        //public int elanio_actual =2019;
+
+       /* public int AniosTrabajados(int anio_actual)
+        {
+            return anio_actual -  fechaingreso.anio;
+
+        }
+
+
+        public int EdadEmpleado(int anio_actual)
+        {
+            return anio_actual - fechanacimiento.anio;
+        }
+
+        public int Antiguedad(int anio_actual)
+        {
+            return anio_actual - fechaingreso.anio;
+        }*/
+
+       /* public void AniosJubilacion(int anio_actual)
+        {
+            int aniosParajubilacion;
+
+            if (genero == elgenero.Masculino)
+            { //si es masculino
+                aniosParajubilacion = 65 - EdadEmpleado(2019);
+                Console.WriteLine("El empleado se jubilara  {0} anios", aniosParajubilacion);
+            }
+
+            if (genero == elgenero.Femenino)
+            {//si es femenino
+                aniosParajubilacion = 60 - EdadEmpleado(2019);
+                Console.WriteLine("El empleado se jubilara  {0} anios", aniosParajubilacion);
+            }
+        }*/
+
+
+
+    }
+
+}
+
